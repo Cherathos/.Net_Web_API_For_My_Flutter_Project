4051870fa439dbb2c637117881bd44d071093b4c
Emir Kayali <a.emirkayali@hotmail.com>
2025-06-01 16:48:09 +0300

    Update

---
+++ API_Project_For_Flutter/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using API_Project_For_Flutter.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Project_For_Flutter.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Furniture> Furnitures { get; set; }
+    }
+}
---
+++ API_Project_For_Flutter/Models/Entities/Furniture.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Project_For_Flutter.Models.Entities
+{
+    public class Furniture
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public required string FurnitureName { get; set; }
+        public IFormFile ? Image { get; set; }
+        public string ? FurnitureType { get; set; }
+        public string ? Description { get; set; }
+        [Required]
+        public required decimal FurniturePrice { get; set; }
+    }
+}
